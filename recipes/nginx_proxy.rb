bee5719b319c5ae7a0c6fb108548e7ba2f1016b6
Jeff Byrnes <jeff@evertrue.com>
2014-08-29 14:15:52 -0400

    Clean up nginx_proxy recipe
    Consistency is key
    
    * Align resource attributes’ arguments
    * Use strings for modes

--- recipes/nginx_proxy.rb
+++ recipes/nginx_proxy.rb
@@ -17,16 +17,14 @@
 # limitations under the License.
 #
 
-# Install an nginx server and proxy trafic to archiva
-
 include_recipe "nginx::#{node[:archiva][:nginx]}"
 
 template 'archiva_server.conf' do
-  path "#{node[:nginx][:dir]}/sites-available/archiva_server.conf"
+  path   "#{node[:nginx][:dir]}/sites-available/archiva_server.conf"
   source "nginx_site_#{node[:archiva][:web_template]}.erb"
-  owner 'root'
-  group 'root'
-  mode 0644
+  owner  'root'
+  group  'root'
+  mode   '0644'
 end
 
 nginx_site 'archiva_server.conf' do
@@ -34,8 +32,8 @@ nginx_site 'archiva_server.conf' do
 end
 
 template "#{node[:archiva][:home]}/conf/jetty.xml" do
-  source 'jetty.xml.erb'
-  mode 0755
-  owner node[:archiva][:user_owner]
-  notifies :restart, resources(service: 'archiva'), :immediately
+  source   'jetty.xml.erb'
+  mode     '0755'
+  owner    node[:archiva][:user_owner]
+  notifies :restart, 'service[archiva]', :immediately
 end
