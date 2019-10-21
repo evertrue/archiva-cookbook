5f26005f8086825a1ae30e12b3901ec32c029e12
Jeff Byrnes <thejeffbyrnes@gmail.com>
2019-10-21 13:21:37 -0400

    Satisfy foodcritic metadata concerns
    FC064: Ensure issues_url is set
    FC065: Ensure source_url is set
    FC066: Ensure chef_version is set
    FC069: Ensure standardized license defined

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
 name             'archiva'
 maintainer       'EverTrue'
 maintainer_email 'devops@evertrue.com'
-license          'Apache v2.0'
+license          'Apache-2.0'
 description      'Installs/Configures Archiva'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
+chef_version     '>= 12.7' if respond_to?(:chef_version)
+source_url       'https://github.com/evertrue/archiva-cookbook'
+issues_url       'https://github.com/evertrue/archiva-cookbook/issues'
 
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '~> 7.2.0'
