71db7fda8f7f21ee9bce0759bfd006cd19066ccd
Doug Youch <dougyouch@gmail.com>
2020-02-12 15:31:45 -0500

    removed version lock on apt

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache-2.0'
 description      'Installs/Configures Archiva'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.0'
+version          '3.0.1'
 chef_version     '>= 12.7' if respond_to?(:chef_version)
 source_url       'https://github.com/evertrue/archiva-cookbook'
 issues_url       'https://github.com/evertrue/archiva-cookbook/issues'
@@ -12,7 +12,7 @@ issues_url       'https://github.com/evertrue/archiva-cookbook/issues'
 supports 'ubuntu', '>= 16.04'
 supports 'debian', '>= 9.0.0'
 
-depends 'apt', '~> 7.2'
+depends 'apt'
 depends 'ark', '~> 4.0'
 depends 'java', '~> 4.3'
 depends 'nginx', '~> 10.0'
