f4e1310b50d3523bbce5d489d458f88c842ff0b3
Doug Youch <dougyouch@gmail.com>
2020-02-12 15:38:44 -0500

    removed version lock for apt

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache v2.0'
 description      'Installs/Configures Archiva'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
 
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '~> 7.2.0'
 
-depends 'apt',   '~> 2.5'
+depends 'apt'
 depends 'ark'
 depends 'java',  '~> 1.27'
 depends 'chef_nginx', '~> 5.0'
